24fc21a155f33f3d7eda7f3a75ef4246af1ca7ba
ApolllonDev <alexeyua79@gmail.com>
2018-05-19 11:34:31 -0400

    Factorial
    In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n.

---
+++ factorial.py
@@ -0,0 +1,22 @@
+from colorama import Fore, Style
+
+class Factorial:
+  # constructor
+  def __init__(self, number):
+    self.number = number
+    try:
+      if int(self.number) >= 0:
+        print(Fore.GREEN + str(self.factorial(int(self.number))) + Style.RESET_ALL)
+      else:
+        raise ValueError()
+    except ValueError:
+      print(Fore.RED + "Input must be greater or equal to zero. Please try again..." + Style.RESET_ALL)
+  
+  # factorial solver
+  def factorial(self, n):
+    if n == 0:
+      return 1
+    else:
+      return n * self.factorial(n-1)
+
+f = Factorial(input())
