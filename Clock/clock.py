96dec5ae509e177363f1e984e455a1225f371848
ApolllonDev <alexeyua79@gmail.com>
2018-05-29 23:26:32 -0400

    Clock project with turtle and time libraries

---
+++ Clock/clock.py
@@ -0,0 +1,73 @@
+import turtle, time
+
+turtles = []
+for i in range(4):
+  turtles.append(turtle.Turtle())
+
+class Clock:
+  # constructor
+  def __init__(self, turtles, size):
+    self.size = size
+    self.t = []
+    for turtle in turtles:
+      turtle.hideturtle()
+      turtle.speed(0)
+      self.t.append(turtle)
+  
+  # draw the clock circle using first turtle
+  def border(self):
+    self.t[0].speed(0)
+    self.t[0].circle(self.size)
+  
+  # draw the clock hours arrow
+  def hours(self):
+    self.t[1].clear()
+    self.t[1].penup()
+    self.t[1].goto(0,self.size)
+    self.t[1].setheading((-1)*(self.get_h()*360/12-90))
+    self.t[1].pendown()
+    self.t[1].pensize(3)
+    self.t[1].fd(self.size*0.6)
+  
+  # draw the clock minutes arrow
+  def minutes(self):
+    self.t[2].clear()
+    self.t[2].penup()
+    self.t[2].goto(0,self.size)
+    self.t[2].setheading((-1)*(self.get_m()*360/60-90))
+    self.t[2].pendown()
+    self.t[2].pensize(2)
+    self.t[2].fd(self.size*0.7)
+  
+  # draw the clock seconds arrow
+  def seconds(self):
+    self.t[3].clear()
+    self.t[3].penup()
+    self.t[3].goto(0,self.size)
+    self.t[3].setheading((-1)*(self.get_s()*360/60-90))
+    self.t[3].pendown()
+    self.t[3].pensize(1)
+    self.t[3].fd(self.size*0.8)
+
+  def get_h(self):
+    return time.localtime(time.time()).tm_hour
+
+  def get_m(self):
+    return time.localtime(time.time()).tm_min
+
+  def get_s(self):
+    return time.localtime(time.time()).tm_sec
+
+def main():  
+  clock = Clock(turtles,100)
+  clock.border()
+
+  while True:
+    turtle.tracer(0,0)
+    clock.hours()
+    clock.minutes()
+    clock.seconds()
+    turtle.tracer(20,0)
+
+main()
+turtle.update()
