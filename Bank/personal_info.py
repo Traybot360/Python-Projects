b9b76336caa029c65c99537d6ba343c3e78268f9
Oleksii Polovyi <alexeyua79@gmail.com>
2018-09-09 15:54:15 -0400

    Update personal_info.py
    ```python
    class X:
      def __init__(self):
        self.attr = "5"
        print("X created: x = " + self.attr)
      def edit(self, attribute, value):
        exec("self."+attribute+ "= value")
        print("X edited: x = " + self.attr)
        
    x = X()
    
    x.edit("attr","10")
    ```
    ___
    ```python
    # Result
    > X created: x = 5
    > X edited: x = 10
    ```

--- Bank/personal_info.py
+++ Bank/personal_info.py
@@ -1,17 +1,17 @@
 class Personal_info:
   def __init__(self,name,lname,pwd,age,joined,gender):
-    self.name = name,
-    self.lname = lname,
-    self.pwd = pwd,
-    self.age = age,
-    self.joined = joined,
-    self.gender = gender,
-    self.attrs = ["name","lname","pwd","age","joined","gender"]
+    self.name = name
+    self.lname = lname
+    self.pwd = pwd
+    self.age = age
+    self.joined = joined
+    self.gender = gender
+    self.attributes = ["name","lname","pwd","age","joined","gender"]
     
-  def edit(self,to_edit,new_val):
-    if(to_edit in self.attrs):
-       a = "self."+to_edit+"="+new_val
+  def edit(self,attribute,value):
+    if(attribute in self.attributes):
+       exec("self."+attribute+"=value")
     else:
-      print("Sorry but the input was invalad")
+      print("Sorry but the input was invalid")
   
     
