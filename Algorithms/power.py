6602ea3e3e671e6dfdf7fd013100c41f9b18152e
Oleksii Polovyi <alexeyua79@gmail.com>
2018-09-22 10:10:54 -0400

    Update power.py

--- Algorithms/power.py
+++ Algorithms/power.py
@@ -1,5 +1,21 @@
-def power(base, n):
+# b is base
+# n is exponent
+def power(b,n):
+  print("runtime")
   if n == 0:
     return 1
-  if n > 0:
-    return base * power(base, n - 1)
+  if n % 2 == 0:
+    return power(b * b, n / 2)
+  else:
+    return b * power(b * b, (n - 1) / 2)
+  
+def power2(b,n):
+  print("runtime")
+  if n == 0:
+    return 1
+  else:
+    return b*power2(b,n-1)
+
+print(power(5,7))
+print()
+print(power2(5,7))
