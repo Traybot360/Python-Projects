8512370205bd3bcec4f6fd40b52d7806f2b17525
Oleksii Polovyi <alexeyua79@gmail.com>
2018-10-03 20:52:04 -0400

    Update main.py

--- Files/main.py
+++ Files/main.py
@@ -17,22 +17,22 @@ with open('log.txt','a') as log:
   books = json.load(books_file)
   books_file.close()
   # Let user view the books
-  log.write("User " + name + " viewed books in at " + get_current_time())
+  log.write("User " + name + " viewed books at " + get_current_time())
   print("----------------")
   print("Available books:")
   print("----------------")
   for book in books:
-    print(str(books.index(book)) + ". " + book["title"])
+    print(str(books.index(book)+1) + ". " + book["title"])
   print("----------------")
 
   # select the book from list
-
-  # remove book from list
-
-  # add new book to the list
-
+  selected = int(input("Select a book:"))
+  print("----------------")
+  print(books[selected-1]["text"])
+  log.write("User " + name + " viewed book " + books[selected-1]["title"] + " at " + get_current_time())
   # save books into file 
-  # with open('books.json', 'w') as outfile:
-  #   json.dumps(books, outfile)  
+  with open('books.json', 'w') as outfile:
+    json.dump(books, outfile)  
+  log.write("Books saved into the file at " + get_current_time())
 
   log.write("User " + name + " logged out at " + get_current_time())
