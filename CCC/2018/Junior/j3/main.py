41665e472f3475dc5618229194c4026ad9e37183
Oleksii Polovyi <alexeyua79@gmail.com>
2018-10-24 16:39:44 -0400

    Update main.py

--- CCC/2018/Junior/j3/main.py
+++ CCC/2018/Junior/j3/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask('app')
 
 # get distances from user
 # distances = input().split()
@@ -46,3 +46,4 @@ def render_table():
   return render_template("table.html", data=data, letters=letters)
 
 if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
