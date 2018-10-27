127ed36ebef37e76e84a48c4c85a51091d78ec7e
Nicholas Singh <nicholas1singh@gmail.com>
2018-10-27 14:35:12 -0400

    Update voronoi.py

--- CCC/2018/Senior/voronoi.py
+++ CCC/2018/Senior/voronoi.py
@@ -1,1 +1,19 @@
+times = int(input())
+nums = [int(input()) for i in range(times)]
+nums = list(reversed(sorted(nums)))
+smallest = ((nums[0]-nums[1])/2) + ((nums[1]-nums[2])/2)
+
+for i in range(1,len(nums)-1):
+  if( (((nums[i-1]-nums[i])/2) + ((nums[i]-nums[i+1]))/2) < smallest ):
+    smallest = (((nums[i-1]-nums[i])/2) + ((nums[i]-nums[i+1]))/2)
+print(smallest)
+
+'''
+5
+16
+0
+10
+4
+15
+'''
 
