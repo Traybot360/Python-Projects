3c11afb3278b7187495e8183832fb2a8d2b413d9
Oleksii Polovyi <alexeyua79@gmail.com>
2018-10-27 10:50:59 -0400

    Update sunflowers.py

--- CCC/2018/Junior/sunflowers.py
+++ CCC/2018/Junior/sunflowers.py
@@ -1,25 +1,53 @@
+# test case 1
+# matrix = [
+#   [1,3],
+#   [2,9]
+# ]
+
+# test case 2
 matrix = [
-	[3, 7, 9],
-	[2, 5, 6],
-	[1, 3, 4]
+  [4,3,1],
+  [6,5,2],
+  [9,7,3]
 ]
 
+# test case 3
+# matrix = [
+# 	[3, 7, 9],
+# 	[2, 5, 6],
+# 	[1, 3, 4]
+# ]
+
+# print the matrix
+def display(data):
+  for row in data:
+	  print(row)
+
+# 90 degrees matrix rotation
 def rotate(data):
   touples = zip(*reversed(data))
   rotated = [list(item) for item in touples]
   return rotated
 
+# True -> valid
+# False -> not valid
 def check(data):
+  # every element in the row must be greater than previous
+  for sublist in data:
+    if sublist != sorted(sublist):
+      return False
+
+  # every first element in column must be greater than previous column
+  sublist = []
   for row in data:
-    for col in row:
-      if row[0] > col:
-        return True
-  return False
+    sublist.append(row[0])
+  
+  if sublist != sorted(sublist):
+    return False
 
-flag = True
-while flag:
-	matrix = rotate(matrix)
-	flag = check(matrix)
+  # list is valid
+  return True
 
-for row in matrix:
-	print(row)
+while not check(matrix):
+  matrix = rotate(matrix)
+display(matrix)
