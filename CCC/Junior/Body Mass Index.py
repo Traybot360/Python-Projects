9dcf0277ff410fc5848d6f09c52d785fb68fb1e9
IceBull06 <44247012+IceBull06@users.noreply.github.com>
2018-11-14 20:18:18 -0500

    Update and rename Body Mass Index to Body Mass Index.py

--- CCC/Junior/Body Mass Index.py
+++ CCC/Junior/Body Mass Index.py
@@ -1,7 +1,7 @@
-x=float(input())
-y=float(input())
-d=y*y
-bmi=x/d
+weight=float(input())
+height=float(input())
+d=height*height
+bmi=weight/d
 if bmi<18.5:
   print("Underweight")
 elif bmi>18.5 and bmi<25.0:
